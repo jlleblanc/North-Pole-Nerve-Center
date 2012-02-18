7a1aafe44fef4791d252fd54002d92defda5e735
Chad Selph <chad.selph@gmail.com>
2012-02-18 13:56:24 -0800

    implemented want

--- npnc/sms/views.py
+++ npnc/sms/views.py
@@ -1,12 +1,40 @@
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
+from accounts.models import UserProfile
 from twilio import twiml
+from gifts.models import WishItem
+import re
 
 
-@csrf_exempt
+# XXX: this should be moved to an authentication
+# provider or a utils function
+def user_from_twilio_request(request):
+    raw_number = request.POST.get('From')
+    if not raw_number:
+        return None
+    match = re.match(r'\+1(\d\d\d)(\d\d\d)(\d\d\d\d)', raw_number)
+    if not match:
+        raise Exception("Phone number aint right")
+    usnumber = "-".join(match.groups())
+    return UserProfile.objects.get(phone_number=usnumber).user
+
+
+def responds_sms(view):
+    def inner(request):
+        response = view(request)
+        twiml_response = twiml.Response()
+        twiml_response.sms(response)
+        return HttpResponse(str(twiml_response))
+    inner.csrf_exempt = True
+    return inner
+
+
+@responds_sms
 def recv_sms(request):
     if request.POST:
+        this_user = user_from_twilio_request(request)
+        if not this_user:
+            return "Sorry, but I don't know who you are!"
         sms_body = request.POST.get('Body', '')
         if " " in sms_body:
             action, arguments = sms_body.split(' ', 1)
@@ -14,36 +42,41 @@ def recv_sms(request):
             action = sms_body
             arguments = ""
         handler = sms_handles.get(action, DEFAULT_HANDLER)
-        twiml_response = twiml.Response()
-        twiml_response.sms(handler(arguments))
-        return HttpResponse(str(twiml_response))
+        return handler(this_user, arguments)
     else:
         return HttpResponse("gotta post, man")
 
 
-def handle_help(rest):
+def handle_help(user, rest):
     return "Commands are: buy <name> <number>, list <name>, who"
 
 
-def handle_buy(rest):
+def handle_buy(user, rest):
     return "you bought %s" % rest
 
 
-def handle_list(rest):
+def handle_list(user, rest):
     return "listing stuff..."
 
 
-def handle_who(rest):
+def handle_who(user, rest):
     users = User.objects.all()
     names = [user.username for user in users]
     return "\n".join(names)
+
+
+def handle_want(user, rest):
+    WishItem.objects.create(item_name=rest, wisher=user)
+    return "Added {0} to your list!".format(rest)
+
 
 
 sms_handles = {
     'buy': handle_buy,
     'list': handle_list,
     'help': handle_help,
-    'who': handle_who
+    'who': handle_who,
+    'want': handle_want,
 }
 DEFAULT_HANDLER = handle_help
 
