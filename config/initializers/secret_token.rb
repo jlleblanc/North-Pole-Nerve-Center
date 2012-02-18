034cd3b475f96c4d648dd30479518a9dd9e85349
Joe LeBlanc <contact@jlleblanc.com>
2012-02-18 11:53:49 -0600

    BYE RAILS!

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Npnc::Application.config.secret_token = '33093eb886ca022670967ede7d1f7ff2bc35bd7d314770004d08da2efa9161e01150bcd1a13b4768ded1d96285fde4b0f166ad7b1b08926834b66162ae1ba8de'
